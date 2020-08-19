c946050217273604de3df0b007dbdf12c56e99c0
takahata-noria <takahatanoria@gmail.com>
2020-08-19 23:06:22 +0900

    89. （解答例）動的ページ作成しよう

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../../products'
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+  product
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')]
+    })
   }
 
 }
