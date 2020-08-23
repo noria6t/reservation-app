4fc1de9dec1201e82f939f4a841743ba477d0890
takahata-noria <takahatanoria@gmail.com>
2020-08-23 23:51:29 +0900

    96. （解答例）商品詳細ページのテキストを動的に対応させよう1,2

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,21 +2,45 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    headingtext1: 'サンプルテキスト---1',
+    headingtext2: 'サンプルテキスト---2',
+    headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    headingtext1: 'サンプルテキスト---1',
+    headingtext2: 'サンプルテキスト---2',
+    headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: 'sss'
+    description: 'sss',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    headingtext1: 'サンプルテキスト---1',
+    headingtext2: 'サンプルテキスト---2',
+    headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
   },
   {
     name: 'Phone Special',
     price: 999,
-    description: 'ccc'
+    description: 'ccc',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    headingtext1: 'サンプルテキスト---1',
+    headingtext2: 'サンプルテキスト---2',
+    headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
   }
 ];
