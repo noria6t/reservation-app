a82b0aad5b717e511423f8f35519933428c6db7c
takahata-noria <takahatanoria@gmail.com>
2020-08-24 13:09:12 +0900

    108. MongoDBへ接続！

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./config/dev')
+mongoose.connect(config.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express()
+
+// コールバックfunctionのfunction(req, res)の形はexpress側のルールでの書き方。/productsというリクエストが来たらsuccessというのを呼び出した下の方で実行してね。
+app.get('/products', function(req, res){
+  res.json({'success': true})
+})
 
 // herokuとかでサーバーのポートが変わっても参照できるように
 const PORT = process.env.PORT || '3001'
@@ -8,3 +19,8 @@ const PORT = process.env.PORT || '3001'
 app.listen(PORT, function(){
   console.log('I am lanning')
 })
+
+// mongoose mongoDBにアクセスする便利なフレームワーク
+// npm install mongoose --save
+// --saveとはインストール情報がpackage.jsonとpackage.lock.json(インストールフレームのインストール情報入ってる)の２つあるのだが、--saveとうつとpackageの情報がlockの方へ記憶される。
+// 新しくチームメンバーが参加して、空っぽの人がsaveなしでnpm install mongooseとうつと、lockの内容がそのままコピーされる
