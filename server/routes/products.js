91090db3250a57c3ea7e6cfb99f65c725a5a05ff
takahata-noria <takahatanoria@gmail.com>
2020-09-13 15:44:23 +0900

    115. Product Endpoint（エンドポイント）作成①

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
+// モデルのproductを読み込み
+const Product = require('../model/product')
 
 router.get('', function(req, res){
-  res.json({'ok': true})
+  // OKじゃなくてDBのproductの情報を返すように変更したい
+  // {} つまり何も中身をないときにerrまたはfoundProductsをレスポンスする。function(req, res)がexpressの書き方
+  Product.find({}, function(err, foundProducts){
+    res.json(foundProducts)
+  })
 })
 
 module.exports = router
