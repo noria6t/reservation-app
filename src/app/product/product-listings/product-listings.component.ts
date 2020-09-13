e70e2fda12a3b0f8e60745477e9611f51c5fc419
takahata-noria <takahatanoria@gmail.com>
2020-09-13 21:02:35 +0900

    124. Observableを使ったサンプルプログラムを作ってみよう

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../shared/product.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -17,6 +18,27 @@ export class ProductListComponent implements OnInit {
     // this.12行目のproducts = 2行目のproducts。HTMLではthis.12行目のproductsを読み込んでいる。（product of productsの部分）
     // productServiceを経由してデータを読み込む
     this.products = this.productService.getProducts()
+
+    // const observable で観測対象を作る。で、next(1)2,3..と読んでいくobsavableを作る。データを放出するためにnextというのを使う。errorかcompleteがきた時はその後をsubscriveが読無のを諦める。
+    const observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.error();
+      subscriber.complete();
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+    // obsavableがどんな動きをすか
+    console.log('just before subscribe');
+    observable.subscribe({
+      next(data) { console.log('次のデータが出力されました' + data); },
+      error(err) { console.error('something wrong occurred: ' + err); },
+      complete() { console.log('done'); }
+    });
+    console.log('just after subscribe');
+    
   }
 
 }
